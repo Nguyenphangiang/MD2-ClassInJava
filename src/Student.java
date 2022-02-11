713954a121e21e7c20e990cc3d8fde7348a82f2e
Nguyenphangiang <phancruel@gmail.com>
2022-02-11 17:53:46 +0700

    up

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    void Student(){
+
+    }
+
+     String setName(String name){
+        return this.name = name;
+    }
+     String setClasses(String classes){
+        return this.classes = classes;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println( s1.setName("Cage"));
+        System.out.println( s1.setClasses("C12"));
+    }
+}
